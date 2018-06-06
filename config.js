baaa20f02f2696d1311e22676c09dc884064e154
EC2 Default User <ec2-user@ip-172-31-57-165.ec2.internal>
2018-06-06 07:27:03 +0000

    Started the web application

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Savant";
+config.admin.email="";
+config.app.name="Savants";
+config.app.url="";
+
+module.exports=config
